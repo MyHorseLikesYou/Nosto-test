e76f9ee5c944b72a7fa98ec5df4fc19500b1803c
Kirill Klochkov <kklochkov@plazius.ru>
2022-02-07 19:46:23 +0300

    chore: add empty project

---
+++ src/main/java/com/example/nostotest/NostoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nostotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nostotest/NostoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nostotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NostoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
